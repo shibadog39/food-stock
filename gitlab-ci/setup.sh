4cc433d9cfb34dd829a5b476d7deb1260c5ef1e4
Sakurai Takahisa <shibadog1121@gmail.com>
2019-05-01 00:20:42 +0900

    fix setup.sh

--- gitlab-ci/setup.sh
+++ gitlab-ci/setup.sh
@@ -1,24 +1,4 @@
-#!/bin/bash -xe
-
-# # Setup ssh key
-# mkdir -p -m 700 ~/.ssh
-
-# #########################
-# # 秘密鍵の内容をコンソールに出したくないので一時的に無効にする
-# set +x
-
-# # NOTE: SSH_PRIVATE_KEYはSettings -> Variablesで設定
-# echo -e "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
-
-# set -x
-# #########################
-
-# chmod 600 ~/.ssh/id_rsa
-
-# cat << EOS > ~/.ssh/config
-# Host *
-#     StrictHostKeyChecking no
-# EOS
+#!/bin/sh
 
 cp gitlab-ci/database.yml config/database.yml
 
