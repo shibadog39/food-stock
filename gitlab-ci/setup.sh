14871465a493ed143af4f88fb73d4d948cfd038b
Sakurai Takahisa <shibadog1121@gmail.com>
2019-05-01 10:07:51 +0900

    test rubocop

--- gitlab-ci/setup.sh
+++ gitlab-ci/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xe
 
-sudo cp gitlab-ci/database.yml config/database.yml
+cp gitlab-ci/database.yml config/database.yml
 
-bundle check --path=${BUNDLE_CACHE} || bundle install --path=${BUNDLE_CACHE} --jobs=2 --retry=3
+bundle install --path=${BUNDLE_CACHE} --jobs=2 --retry=3
 
 bundle exec rails db:create
 bundle exec rails db:migrate:reset
