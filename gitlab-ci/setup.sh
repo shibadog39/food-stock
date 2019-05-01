3418ab8deda05e2516abe3fa1916f15a9505d1b9
Sakurai Takahisa <shibadog1121@gmail.com>
2019-05-01 10:50:24 +0900

    fix gitlab ci

--- gitlab-ci/setup.sh
+++ gitlab-ci/setup.sh
@@ -3,7 +3,7 @@
 cp gitlab-ci/database.yml config/database.yml
 
 gem install bundler
-bundle install --jobs=2 --retry=3
+bundle install
 
 bundle exec rails db:create
 bundle exec rails db:migrate:reset
